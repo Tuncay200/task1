4148d2f7e6a2b8cf6962ce2c6940d9f132cdf931
Tuncay200 <tuncayadisirinli528@gmail.com>
2024-05-17 20:42:24 +0400

    dafd

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public  static  void main(){
         int a =5;
         int b=3;
-        int z =a +b;
-        System.out.println(z);
+        int ss =a +b;
+        System.out.println(ss);
     }
 }
